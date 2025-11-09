0439bbb71f6fbd6d869b5de8518a8f3c3d09ac0d
premsanjaysha <sanjaysha1803@gmail.com>
2025-11-09 14:13:37 +0530

    chngd aws

--- app3.py
+++ app3.py
@@ -22,8 +22,8 @@ S3_BUCKET = os.getenv("S3_BUCKET", "s3myfirsttesting")
 S3_REGION = os.getenv("AWS_DEFAULT_REGION", "us-east-1")
 s3 = boto3.client(
     's3',
-    aws_access_key_id='aws_access_key_id',
-    aws_secret_access_key='aws_secret_access_key',
+    aws_access_key_id=os.getenv("aws_access_key_id"),
+    aws_secret_access_key=os.getenv("aws_secret_access_key"),
     region_name=S3_REGION
 )
 
